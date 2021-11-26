73bed0e5001c8291366e1ff6a388ca8121ac9fe9
Vivektkofficial <vivektkofficial@gmail.com>
2021-11-26 22:55:41 +0530

    codpush test

--- Reactcodepush/App.js
+++ Reactcodepush/App.js
@@ -72,7 +72,7 @@ const App: () => Node = () => {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen1234122345166 and then come back to see your edits.
+          code push works.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
